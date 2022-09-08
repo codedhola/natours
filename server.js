e9c3c21187ccfc063b9cfd27078849740bf076a7
codedhola <codedhola2000@gmail.com>
2022-09-08 19:26:18 +0100

    refractor code

--- server.js
+++ server.js
@@ -5,21 +5,22 @@ const app = express();
 
 app.use(express.json());
 
-const fileData = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, "utf-8");
+const fileData = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, "utf-8"));
 
 
-app.get("/api/v1/tours", (req, res) => {
-    const data = JSON.parse(fileData);
+const getAllTours = (req, res) => {
+    const data = fileData;
+    console.log(req.timer)
     res.status(200).json({
         "status" : "success",
         "length" : data.length,
         "data" : data
     });
-});
+};
 
-app.post("/api/v1/tours", (req, res) => {
+const createTour = (req, res) => {
     const getTours = req.body;
-    const allData = JSON.parse(fileData);
+    const allData = fileData;
 
     // TOURS TO ADD 
     const id = allData.length;
@@ -31,7 +32,68 @@ app.post("/api/v1/tours", (req, res) => {
     });
     
     res.send(tour); 
-});
+};
+
+const getTourById = (req, res) => {
+    const param = req.params;
+    const tour = fileData.find(el => el.id == param.id);
+    //  TOUR RECEIVES A PARTICULAR ID TOUR
+    if(!tour){
+        return res.status(404).send({
+            status: "unsuccessful",
+            message: "couldn't find the tour with the given id"
+        })
+    }
+    res.status(200).send({
+        status: "success",
+        data: {
+            tour
+        }
+    });
+}
+
+const editTour = (req, res) => {
+    const param = req.params;
+    const tour = fileData.find(el => el.id == param.id);
+
+    if(!tour){
+        return res.status(404).send({
+            status: "unsuccessful",
+            message: "Couldn't Find the tour to edit"
+        })
+    }
+    res.send("EDITING");
+};
+
+const deleteTour = (req, res) => {
+    const param = req.params;
+    const tour = fileData.find(el => el.id == param.id);
+
+    if(!tour){
+        return res.status(404).send({
+            status: "unsuccessful",
+            message: "Couldn't Find the tour to delete"
+        })
+    }
+    res.send("DELETING");
+};
+
+// GET ALL TOUR
+app.get("/api/v1/tours", getAllTours);
+
+// CREATE A TOUR
+app.post("/api/v1/tours", createTour);
+
+// GET A TOUR BY ID
+app.get("/api/v1/tours/:id", getTourById);
+
+// EDIT A TOUR BY ID
+app.patch("/api/v1/tours/:id", editTour);
+
+// DELETE A TOUR BY ID
+app.delete("/api/v1/tours/:id", deleteTour);
+
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server instance successful...");
