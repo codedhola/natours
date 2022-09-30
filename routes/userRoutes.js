701671de1418149a03c0ed13c21e7c3c49b8a330
codedhola <codedhola2000@gmail.com>
2022-09-30 17:08:04 +0100

    corrected deletion mistake

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,20 @@ const router = express.Router();
 const { getAllUsers, createUser, getUserById, editUser, deleteUser } = require("../controllers/userControllers");
 
 
+// GET ALL USER
+router.get("/", getAllUsers);
+
+// CREATE A USER
+router.post("/", createUser);
+
+// GET A USER BY ID
+router.get("/:id", getUserById);
+
+// EDIT A USER BY ID
+router.patch("/:id", editUser);
+
+// DELETE A USER BY ID
+router.delete("/:id", deleteUser);
+
 
 module.exports = router;
