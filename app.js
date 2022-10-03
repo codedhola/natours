982e3134731c495b06537e7347b3ea7a25a0fdd9
codedhola <codedhola2000@gmail.com>
2022-10-03 21:49:07 +0100

    feat: included mongoose, refactor: improve structure

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const tourRoutes = require("./routes/tourRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/api/v1/tours", tourRoutes);
+app.use("/api/v1/users", userRoutes);
+console.log(process.env)
+
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv").config({path: "./config.env"});
-const morgan = require("morgan");
 const app = require("./app");
-const tourRoutes = require("./routes/tourRoutes");
-const userRoutes = require("./routes/userRoutes");
-
-
-app.use(express.json());
-app.use(morgan("dev"));
-
-app.use("/api/v1/tours", tourRoutes);
-app.use("/api/v1/users", userRoutes);
-console.log(process.env)
 
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
     console.log("Server instance successful... on port " + port);
 })
