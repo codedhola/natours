8d2c87c8cbcf66e6ea8f576df9480365675d3518
codedhola <codedhola2000@gmail.com>
2022-10-23 18:30:52 +0100

    BUG: Fix Error handler in AppError class

--- app.js
+++ app.js
@@ -17,10 +17,12 @@ app.use("/api/v1/tours", tourRoutes);
 app.use("/api/v1/users", userRoutes);
 
 app.all("*", (req, res, next) => {
-    const err = new Error(`Route ${req.originalUrl} can't be found on the server`);
-    err.statusCode = 404;
-    err.status = "fail";
+    const err = new AppError(`Route ${req.originalUrl} can't be found on the server`, 404);  // Better Error Handler
     next(err);
+    
+    // const err = new Error(`Route ${req.originalUrl} can't be found on the server`);
+    // err.statusCode = 404;
+    // err.status = "fail";
 });
 
 app.use((err, req, res, next) => {
--- utils/appError.js
+++ utils/appError.js
@@ -3,7 +3,7 @@ class AppError extends Error {
         super(message);
 
         this.statusCode = statusCode;
-        this.status = `${statusCode}.startswith("4) ? "fail" : "error"`;
+        this.status = `${statusCode}.startswith("4")` ? "Fail" : "Error";
         this.isOperational = true;
 
         Error.captureStackTrace(this, this.constructor);
