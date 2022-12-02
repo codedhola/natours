60b85e7c689bfe6989808b091b9a3653d177dd40
coded hola <codedhola2000@gmail.com>
2022-12-02 19:41:06 +0100

    feat: added casterror in production, fix: production bug in package.json

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,5 @@
+const AppError = require("../utils/appError")
+
 const development = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -9,7 +11,7 @@ const development = (err, res) => {
 
 const production = (err, res) => {
     if(err.isOperational){
-        res.status(statusCode).json({
+        res.status(err.statusCode).json({
             status: err.status,
             message: err.message
         })
@@ -23,6 +25,12 @@ const production = (err, res) => {
     }
 }
 
+function handleCastError(err){
+    const message = `invalid ${err.path}: ${err.value}`
+    return new AppError(message, 400)
+}
+
+
 module.exports = (err, req, res, next) => {
      err.statusCode = err.statusCode || 500;
      err.status = err.status || "Error"
@@ -30,6 +38,8 @@ module.exports = (err, req, res, next) => {
     if(process.env.NODE_ENV !== "production"){
         development(err, res)
     }else {
-        production(err, res)
+        let error = {...err}
+        if(error.name = "CastError") error = handleCastError(err)
+        production(error, res)
     }      
 }
