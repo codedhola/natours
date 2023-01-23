671f8f4b4d6041bb7200f7c6293bc7964040f4d1
codedhola <codedhola2000@gmail.com>
2023-01-23 12:33:44 +0100

    config🛠️:: configured multer for image upload

---
+++ utils/multer.js
@@ -0,0 +1,32 @@
+const multer = require("multer")
+const AppError = require("./appError")
+
+const multerStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "/public/img/users")
+    },
+    filename: (req, file, cb) => {
+        const ext = file.mimetype.split("/")[1]
+        cb(null, `user-${req.user}-${Date.now()}.${ext}`)
+    }
+})
+
+const multerFilter = (req, file, cb) => {
+    if(file.mimetype.startsWith("image")){
+        cb(null, true)
+    }else{
+        cb(new AppError("Not a valid image file", 400))
+    }
+}
+
+const upload = multer({
+    storage: multerStorage,
+    fileFilter: multerFilter
+})
+
+
+const uploadUserPhoto = upload.single("photo")
+
+module.exports = {
+    uploadUserPhoto
+}
