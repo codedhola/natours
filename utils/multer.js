fed8a1660805d3adf7d5af10b1e6682dd877434d
codedhola <codedhola2000@gmail.com>
2023-01-27 23:27:57 +0100

    fix🔧: fixed async issue in user upload

--- utils/multer.js
+++ utils/multer.js
@@ -31,11 +31,11 @@ const upload = multer({
 
 const uploadUserPhoto = upload.single("photo")
 
-const resizeUserPhoto = (req, res, next) => {
+const resizeUserPhoto = async (req, res, next) => {
     if(!req.file) return next()
     req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`
     console.log(req.file.filename)
-    sharp(req.file.buffer).resize(500, 500).toFormat('jpeg').jpeg({quality: 90}).toFile(`public/img/users/${req.file.filename}`)
+    await sharp(req.file.buffer).resize(500, 500).toFormat('jpeg').jpeg({quality: 90}).toFile(`public/img/users/${req.file.filename}`)
 
     next()
 }
@@ -45,9 +45,35 @@ const uploadTourPhoto = upload.fields([
     { name: "images", maxCount: 3}
 ])
 
-const resizeTourPhoto = (req, res, next) => {
-    console.log(req.files)
-    next()
+const resizeTourPhoto = async (req, res, next) => {
+    if (!req.files.imageCover || !req.files.images) return next();
+
+  // 1) Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  // 2) Images
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    })
+  );
+
+  next();
 }
 
 module.exports = {
