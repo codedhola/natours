746a496286d8a17130b7b1a18253212411a6e8c8
codedhola <codedhola2000@gmail.com>
2023-02-07 14:16:11 +0100

    feat📧: completed email feature using gmail service

--- utils/mailTo.js
+++ utils/mailTo.js
@@ -14,10 +14,10 @@ module.exports = class Email{
     if (process.env.NODE_ENV === 'production') {
       // Sendgrid
       return nodemailer.createTransport({
-        service: 'SendGrid',
+        service: 'Gmail',
         auth: {
-          user: process.env.SENDGRID_USERNAME,
-          pass: process.env.SENDGRID_PASSWORD
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD
         }
       });
     }
