4a1c5a19d89808a1ca7de252359a92039fe3c541
codedhola <codedhola2000@gmail.com>
2023-02-08 09:22:24 +0100

    deploy🚀: configured database and code for deployment

--- server.js
+++ server.js
@@ -4,15 +4,44 @@ const mongoose = require("mongoose");
 
 const port = process.env.PORT || 3000;
 mongoose.set('strictQuery', false);
-mongoose.connect(process.env.CONNECT_MONGOOSE).then(() => {
-    const server = app.listen(port, () => {
-        console.log("Server Running... on port " + port);
+
+if(process.env.NODE_ENV === "development"){
+    mongoose.connect(process.env.CONNECT_MONGOOSE).then(() => {
+        const server = app.listen(port, () => {
+            console.log("Server Running... on port " + port);
+        })
+        
+        process.on("uncaughtException", err => {
+            console.log(err.name, err.message)
+            server.close(() => {
+                process.exit(1)
+            })
+        })
     })
+}else{
+    const connectionParams = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true 
+    }
+    const mongooseConnect = process.env.CONNECT_CLOUD.replace(/<password>/, () =>  process.env.PASSKEY);
     
+
+    mongoose.connect(mongooseConnect, connectionParams)
+        .then( () => {
+            console.log('Connected to the database ')
+        })
+        .catch( (err) => {
+            console.error(`Error connecting to the database. n${err}`)
+        })
+
+        app.listen(port, () => {
+            console.log("Server Running... on port " + port)
+    })
+
     process.on("uncaughtException", err => {
         console.log(err.name, err.message)
         server.close(() => {
             process.exit(1)
         })
     })
-})
+}
